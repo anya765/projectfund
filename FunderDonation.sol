872659bfca2787398012bb5dbbed76744d750c64
Anya Singh <anyasingh070605@gmail.com>
2022-06-18 21:56:27 -0400

    Add files via upload

---
+++ FunderDonation.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: agpl-3.0
+
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol";
+import "@aave/protocol-v2/contracts/interfaces/ILendingPoolAddressesProvider.sol";
+
+contract FunderDonation {
+    ILendingPoolAddressesProvider addressesProvider;
+    ILendingPool lendingPool;
+
+    // USDC Address on Kovan
+    ERC20 usdc = ERC20(0xe22da380ee6B445bb8273C81944ADEB6E8450422);
+    uint16 referralCode = 0;
+
+    // Constructor takes the address of the AAVE protocol addresses provider. Should not change once deployed.
+    constructor(address _lendingPoolAddressesProvider) public {
+        addressesProvider = ILendingPoolAddressesProvider(
+            _lendingPoolAddressesProvider
+        );
+        lendingPool = ILendingPool(addressesProvider.getLendingPool());
+    }
+
+    // Signals to the front end whether or not the donation was successful
+    event DonationCompleted(bool success);
+
+    function donate(uint256 amount, address to) external {
+        require(amount > 0, "Amount needs to be greater than 0");
+
+        // Transfer USDC to this contract
+        usdc.transferFrom(msg.sender, address(this), amount);
+
+        // Approve contract to deposit it into the lendingPool
+        usdc.increaseAllowance(address(lendingPool), amount);
+
+        // Actually deposit into the lendingPool on behalf of the project
+        lendingPool.deposit(address(usdc), amount, to, referralCode);
+
+        emit DonationCompleted(true);
+    }
+}
